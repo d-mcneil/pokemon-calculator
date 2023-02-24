d54011289111d71d3bccf4edb591e09f3ca30885
Dean McNeil <deanolmcneil@gmail.com>
2023-02-24 14:53:31 -0500

    Manage when a field is left blank of if an input is out of range

--- src/components/CalculatorField/CalculatorField.component.jsx
+++ src/components/CalculatorField/CalculatorField.component.jsx
@@ -6,11 +6,11 @@ import { updateCalculatorField } from "../../redux/actions";
 import "./CalculatorField.styles.scss";
 
 const mapDispatchToProps = (dispatch) => ({
-  onChange: (...args) => dispatch(updateCalculatorField(...args)),
+  updateCalculatorField: (...args) => dispatch(updateCalculatorField(...args)),
 });
 
 const CalculatorField = ({
-  onChange,
+  updateCalculatorField,
   inputType = "number",
   maxValue = 252,
   minValue = 0,
@@ -18,10 +18,42 @@ const CalculatorField = ({
   statLabel, // hp, attack, defense, specialAttack, specialDefense, speed, level
   fieldType, // baseStat, currentStat, iv, ev, natureModifier, level
 }) => {
+  const onChange = (event) => {
+    const target = event.target;
+    let value = target.value;
+    const validateValues = () => {
+      if (value < minValue) {
+        target.value = minValue;
+      } else if (value > maxValue) {
+        target.value = maxValue;
+      } else {
+        target.value = Math.round(value);
+      }
+    };
+    // eslint-disable-next-line
+    if (value == 0) {
+      // don't calculate anything if a field is left blank
+      return;
+    }
+    validateValues();
+    updateCalculatorField(fieldType, statLabel, target.value);
+  };
+
+  const onBlur = (event) => {
+    // the minimum value will be placed in the field if the field is empty when clicked off of
+    const target = event.target;
+    let value = target.value;
+    // eslint-disable-next-line
+    if (value == 0) {
+      target.value = minValue;
+    }
+  };
+
   return (
     <>
       <input
-        onChange={(event) => onChange(fieldType, statLabel, event.target.value)}
+        onChange={onChange}
+        onBlur={onBlur}
         type={inputType}
         max={maxValue}
         min={minValue}
